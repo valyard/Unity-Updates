df429f12f31481c6c2d29c50012196d89406c4ba
Valentin Simonov <v@lent.in>
2015-12-24 17:51:48 +0600

    Added individual Coroutines instead of Update test.

---
+++ Assets/Scripts/CoroutineBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoroutineBehavior : MonoBehaviour {
+
+    private int i;
+
+	private void Start() 
+    {
+	   StartCoroutine(MyUpdate());
+	}
+    
+    private IEnumerator MyUpdate()
+    {
+        while (true)
+        {
+            yield return null;
+            i++;
+        }
+    }
+}
